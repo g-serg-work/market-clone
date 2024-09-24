8a4bd67585d6dbd8a7e15a289eb88df4133082fe
g-serg-work <gor.serg.work77@gmail.com>
2024-09-24 17:19:54 +0300

    NotFoundPage fix

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,23 +1,31 @@
-import { useTranslation } from 'react-i18next';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './NotFoundPage.module.scss';
+import Svg from '@/shared/assets/icons/404.svg';
 
 interface NotFoundPageProps {
     className?: string;
 }
 
 export const NotFoundPage = ({ className }: NotFoundPageProps) => {
-    const { t } = useTranslation();
-    /*Тут ничего нет
-Попробуйте вернуться назад или поищите что-нибудь другое.
-Скидки и акции
-На главную*/
     return (
         <div
             data-testid="NotFoundPage"
             className={classNames(cls.NotFoundPage, {}, [className])}
         >
-            {t('Страница не найдена')}
+            <Svg />
+            <header className={cls.header}>Тут ничего нет</header>
+            <section className={cls.section}>
+                <div>
+                    Попробуйте вернуться назад или поищите что-нибудь другое.
+                </div>
+                <div>Код ошибки: 404</div>
+            </section>
+
+            <div className={cls.linkWrapper}>
+                <a href="/">
+                    <span>На главную</span>
+                </a>
+            </div>
         </div>
     );
 };
