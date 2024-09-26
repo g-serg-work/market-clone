c1dc5fa2f5c2d7336f688d9fc74fdfa4d235032a
g-serg-work <gor.serg.work77@gmail.com>
2024-09-26 16:22:33 +0300

    Modal ui change

--- src/shared/types/custom.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseSyntheticEvent } from 'react';
-
-export type AnchorEvent = BaseSyntheticEvent<
-    MouseEvent,
-    EventTarget & HTMLAnchorElement,
-    EventTarget
->;
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,13 +1,13 @@
-import { ReactNode } from 'react';
+import { MouseEvent, ReactNode, useCallback } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import { useModal } from '@/shared/lib/hooks/useModal/useModal';
 import { useTheme } from '@/shared/lib/hooks/useTheme/useTheme';
 import cls from './Modal.module.scss';
-import { Overlay } from '../Overlay';
 import { Portal } from '../Portal';
+import { ModalTabIndexInitial } from './ModalTabIndexInitial';
 
 export interface ModalProps {
-    className?: string;
+    className: string;
     children?: ReactNode;
     isOpen?: boolean;
     onClose?: () => void;
@@ -16,8 +16,19 @@ export interface ModalProps {
 
 const ANIMATION_DELAY = 180;
 
+/**
+ * Need use as
+ *
+ * [className] {
+ *
+ *  [data-modal-role='overlay'] { ...  }
+ *
+ *  [data-modal-role='content'] { ...  }
+ *
+ * }
+ */
 export const Modal = (props: ModalProps) => {
-    const { className, children, isOpen, onClose, lazy } = props;
+    const { className, children: content, isOpen, onClose, lazy } = props;
 
     const { close, isClosing, isMounted } = useModal({
         animationDelay: ANIMATION_DELAY,
@@ -26,6 +37,13 @@ export const Modal = (props: ModalProps) => {
     });
 
     const { theme } = useTheme();
+
+    const onContentClick = useCallback((e: MouseEvent<HTMLDivElement>) => {
+        const target = e.target as HTMLDivElement;
+        if (target.dataset?.modalRole === 'close-node') {
+            close();
+        }
+    }, []);
 
     const mods: Mods = {
         [cls.opened]: isOpen,
@@ -38,15 +56,19 @@ export const Modal = (props: ModalProps) => {
 
     return (
         <Portal element={document.getElementById('app') ?? document.body}>
-            <div
-                className={classNames(cls.Modal, mods, [
-                    className,
-                    theme,
-                    'app_modal',
-                ])}
-            >
-                <Overlay onClick={close} />
-                <div className={cls.content}>{children}</div>
+            <div className={classNames(className, mods, [theme])}>
+                <div data-modal-role="overlay" onClick={onClose} />
+                <div data-modal-role="content" className={cls.content}>
+                    <ModalTabIndexInitial tabIndex={1} />
+                    <div
+                        data-modal-role="close-node"
+                        className={cls.animation}
+                        onClick={onContentClick}
+                    >
+                        {content}
+                    </div>
+                    <ModalTabIndexInitial tabIndex={0} />
+                </div>
             </div>
         </Portal>
     );
