1ad78929afc23a55135cc615e0f78f715320191c
g-serg-work <gor.serg.work77@gmail.com>
2024-09-26 17:17:53 +0300

    names long export path fix

---
+++ src/shared/lib/hooks/index.ts
@@ -0,0 +1,8 @@
+export { useAppDispatch } from './useAppDispatch/useAppDispatch';
+export { useDebounce } from './useDebounce/useDebounce';
+export { useHover } from './useHover/useHover';
+export { useInfiniteScroll } from './useInfiniteScroll/useInfiniteScroll';
+export { useInitialEffect } from './useInitialEffect/useInitialEffect';
+export { useModal } from './useModal/useModal';
+export { useTheme } from './useTheme/useTheme';
+export { useThrottle } from './useThrottle/useThrottle';
