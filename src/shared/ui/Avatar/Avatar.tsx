14d140be903d22cd73d2d785193b119a6168541b
g-serg-work <gor.serg.work77@gmail.com>
2024-09-26 18:45:13 +0300

    Avatar add

---
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -0,0 +1,33 @@
+import { classNames } from '@/shared/lib/classNames/classNames';
+import cls from './Avatar.module.scss';
+import Png from '../../assets/icons/avatar.png';
+
+interface AvatarProps {
+    className?: string;
+    userName?: string;
+    hasNotification?: boolean;
+    onClick?: () => void;
+}
+
+export const Avatar = (props: AvatarProps) => {
+    const { className, userName, hasNotification, onClick } = props;
+
+    return (
+        <div
+            className={classNames(cls.Avatar, {}, [className])}
+            data-zone-name="profile"
+            data-baobab-name="profile"
+        >
+            <button
+                className={cls.button}
+                aria-haspopup="true"
+                aria-controls="userMenu"
+                aria-expanded="false"
+                onClick={onClick}
+            >
+                <img src={Png} alt={userName} />
+            </button>
+            {hasNotification && <span className={cls.notification}></span>}
+        </div>
+    );
+};
