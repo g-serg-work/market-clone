ad770723983f79fa65b99c674916c3638bdbdb7f
g-serg-work <gor.serg.work77@gmail.com>
2024-10-01 13:40:47 +0300

    plus header item layout fix

--- src/shared/ui/PlusHeaderDesktopItem/PlusHeaderDesktopItem.tsx
+++ src/shared/ui/PlusHeaderDesktopItem/PlusHeaderDesktopItem.tsx
@@ -1,10 +1,50 @@
 import { memo } from 'react';
 import classNames from '@/shared/lib/classNames';
 import cls from './PlusHeaderDesktopItem.module.scss';
+import Svg from '../../assets/icons/ya-plus-badge.svg';
 
 export interface PlusHeaderDesktopItemProps {
     className?: string;
 }
+
+const icon = (
+    <div className={cls.iconContainer} aria-hidden="true">
+        <div className={cls.iconWrapper}>
+            <div className={cls.iconParent} data-zone-name="yaPlusBadge">
+                <Svg
+                    className={cls.badge}
+                    aria-hidden="true"
+                    aria-label="Яндекс Плюс"
+                    data-auto="yaPlusBadge"
+                ></Svg>
+            </div>
+        </div>
+    </div>
+);
+
+const link = (
+    <a
+        className={cls.link}
+        href="https://plus.yandex.ru"
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+        data-auto="headerPlusBalance"
+        aria-label="Баллы Плюса"
+    >
+        <div className={cls.linkContent}>
+            {icon}
+            <div
+                className={cls.textContainer}
+                role="alert"
+                aria-live="polite"
+                aria-atomic="true"
+            >
+                Баллы
+                <span className="ds-visuallyHidden"></span>
+            </div>
+        </div>
+    </a>
+);
 
 export const PlusHeaderDesktopItem = memo(
     (props: PlusHeaderDesktopItemProps) => {
@@ -15,8 +55,10 @@ export const PlusHeaderDesktopItem = memo(
                 className={classNames(cls.PlusHeaderDesktopItem, {}, [
                     className,
                 ])}
+                data-zone-name="HeaderPlusBadge"
+                data-baobab-name="plusAmount"
             >
-                Баллы
+                {link}
             </div>
         );
     },
