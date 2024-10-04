8ae5ebbd5ca94dc479e409a494ea7b4b0e64aae2
g-serg-work <gor.serg.work77@gmail.com>
2024-10-04 16:45:16 +0300

    UserProfileModal autoFocus fix

--- src/features/UserProfile/ui/UserProfileModal/UserProfileModal.tsx
+++ src/features/UserProfile/ui/UserProfileModal/UserProfileModal.tsx
@@ -29,7 +29,7 @@ export const UserProfileModal = (props: UserProfileModalProps) => {
             onClose={onClose}
             lazy
         >
-            <UserProfile />
+            <UserProfile autoFocus />
         </Modal>
     );
 };
