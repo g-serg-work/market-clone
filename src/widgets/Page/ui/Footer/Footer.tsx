782d46d09ad61951e7a3b0ae3b46bf747c31a6cf
g-serg-work <gor.serg.work77@gmail.com>
2024-11-12 18:20:00 +0300

    Footer support elementOnScreen event add

--- src/widgets/Page/ui/Footer/Footer.tsx
+++ src/widgets/Page/ui/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import classNames from '@/shared/lib/helpers/classNames';
 import cls from './Footer.module.scss';
@@ -13,6 +13,11 @@ import { GeneratedQrCode } from './ui/Footer.GeneratedQrCode';
 import { AppGalleryInstall } from './ui/Footer.AppGalleryInstall';
 import { AppStoreInstall } from './ui/Footer.AppStoreInstall';
 import { GooglePlayInstall } from './ui/Footer.GooglePlayInstall';
+import { useElementOnScreen } from '@/shared/lib/hooks/useElementOnScreen';
+import {
+    elementOnScreenChannel,
+    elementOnChannelEvent,
+} from '@/shared/eventsChannels/onScreenEventsChannel';
 
 export interface FooterProps {
     className?: string;
@@ -23,8 +28,25 @@ export const Footer = memo((props: FooterProps) => {
 
     const userData = useSelector(getUserData);
 
+    const onChangeOnScreen = useCallback((onScreen: boolean) => {
+        elementOnScreenChannel.emit(
+            elementOnChannelEvent.footerOnScreen,
+            onScreen,
+        );
+    }, []);
+
+    const [containerRef] = useElementOnScreen<HTMLDivElement>(
+        {
+            // root: window
+            rootMargin: '0px',
+            threshold: 0,
+        },
+        onChangeOnScreen,
+    );
+
     return (
         <div
+            ref={containerRef}
             id="footer"
             className={classNames(cls.Footer, {}, [className])}
             data-apiary-widget-id="/footer"
